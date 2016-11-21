fc63f9a02c077d47516ed05c3b596dea8a4b462c
shigemk2 <i.am.shige@gmail.com>
2016-11-21 23:41:29 +0900

    Add WorkConsumer

--- src/main/scala/com/example/PollingConsumer.scala
+++ src/main/scala/com/example/PollingConsumer.scala
@@ -7,3 +7,34 @@ object PollingConsumerDriver extends CompletableApp(1) {
 
 case class WorkNeeded()
 case class WorkOnItem(workItem: WorkItem)
+
+class WorkConsumer(workItemsProvider: ActorRef) extends Actor {
+  var totalItemsWorkedOn = 0
+
+  def performWorkOn(workItem: WorkItem) = {
+    totalItemsWorkedOn = totalItemsWorkedOn + 1
+    if (totalItemsWorkedOn >= 15) {
+      context.stop(self)
+      PollingConsumerDriver.completeAll
+    }
+  }
+
+  override def postStop() = {
+    context.stop(workItemsProvider)
+  }
+
+  def receive = {
+    case allocated: WorkItemsAllocated =>
+      println("WorkItemsAllocated...")
+      allocated.workItems map { workItem =>
+        self ! WorkOnItem(workItem)
+      }
+      self ! WorkNeeded()
+    case workNeeded: WorkNeeded =>
+      println("WorkNeeded...")
+      workItemsProvider ! AllocateWorkItems(5)
+    case workOnItem: WorkOnItem =>
+      println(s"Performed work on: ${workOnItem.workItem.name}")
+      performWorkOn(workOnItem.workItem)
+  }
+}
